550c7805170532683e6e5fee94688c7a56f369e9
Dipinoar <113305509+Dipinoar@users.noreply.github.com>
2023-06-21 18:21:48 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -59,12 +59,12 @@ resource "aws_security_group" "devops_sg" {
     }
 
     ingress {
-        from_port   = 27017
-        to_port     = 27017
-        protocol    = "tcp"
+        from_port   = 8472
+        to_port     = 8472
+        protocol    = "udp"
         cidr_blocks = ["0.0.0.0/0"]
     }
-
+   
     egress {
         from_port   = 0
         to_port     = 0
